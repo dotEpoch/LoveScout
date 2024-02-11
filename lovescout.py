f5d5436ff2cc50299ed1a2af84672d7f9dd17a09
kyndaa <kynda@nashif.org>
2024-02-10 20:02:30 -0500

    added magenta button

--- lovescout.py
+++ lovescout.py
@@ -9,10 +9,20 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
+from kivy.uix.floatlayout import FloatLayout
+    
 
 class MainApp(App):
     def build(self):
-        self.temp = ['blah']
+        
+        #create start button
+        Fl = FloatLayout()
+        btn = Button(text = 'Click to begin your adventure!', size_hint = (.3, .5),
+                     background_color = (1,0,0.592,1), pos_hint={'x':.3, 'y':.2})
+        Fl.add_widget(btn)
+        return Fl
+        
+        
         
 if __name__ == "__main__":
     app = MainApp()
