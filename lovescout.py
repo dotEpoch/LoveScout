ee839887c8246b8067cfcbb2ec1a0abe0106776a
kyndaa <kynda@nashif.org>
2024-02-11 11:13:03 -0500

    done wokring app

--- lovescout.py
+++ lovescout.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from kivy.app import App
+from kivy.core.window import Window
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
+#from kivy.config import Config
 from kivy.uix.screenmanager import ScreenManager,Screen
 from openai import OpenAI
+#Config.set('graphics','resizable', True)
 
 """
 AI functions
@@ -14,13 +17,20 @@ def get_prompt():
     
     client = OpenAI(api_key = "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")
     
-    prompt = "Generate an outdoors scavenger hunt in the city of Montreal. A picture must be taken of the main attraction of each activity to confirm it's completion. Pictures of animal wildlife will be taken in parks. Insert these prompts into a csv format, seperated by a semi-colon, followed by  the general category of the subject of the photograph. The general categories are Sculpture, Animal, Landmark, Building, Food. Specify the exact element corresponding to their category in parenthesis. An example array is as follows: \"Visit Mount Royal Park and take a photo of the iconic Chalet du Mont Royal; Building (chalet); Visit Schwartz's Deli and try their famous smoked meat sandwich; Food (sandwich). Do not talk during your response."    
+    prompt = "Generate an outdoors scavenger hunt in the city of Montreal. A picture must be taken of the main attraction of each activity to confirm it's completion. Pictures of animal wildlife will be taken in parks. Insert these prompts into a csv format, seperated by a semi-colon, followed by  the general category of the subject of the photograph. The general categories are Sculpture, Animal, Landmark, Building, Food. Specify the exact element corresponding to their category in parenthesis. An example array is as follows: \"Visit Mount Royal Park and take a photo of the iconic Chalet du Mont Royal; Building (chalet); Visit Schwartz's Deli and try their famous smoked meat sandwich; Food (sandwich). Do not talk during your response.Do not add anything after the csv text"    
     chat_completion = client.chat.completions.create(messages=[{"role":"user", "content":prompt}],
                                                  model="gpt-3.5-turbo")
 
     csv = chat_completion.choices[0].message.content
-    response = csv.split(';')
+    response = []
     
+    start = 0
+    for i in range(len(csv)-1):
+        if csv[i] == '\n':
+            response.append(csv[start:i].split(';'))
+            start = i+1
+            
+    response.append(csv[start:].split(';'))
     return response
 
 
@@ -37,22 +47,33 @@ class SecondWindow(Screen):
     def submission(self, filename):
         
         #Get submission
-        global count
+        #global count
         treasure = self.ids.my_image.source = filename[0]
         global prompts
-        prompt_type = prompts[count+1]
-        count+=1
-        
+        global idx
+        print("***********************************************")
+        print(idx)
+        print(len(prompts))
+        print(prompts[-1])
+        print(prompts[idx])
+        print("****************************************")
+        prompt = prompts[idx][0]
+        prompt_type = prompts[idx][1]
+        #count+=1
+        valid = True
         #Check if valid
-        
-        
-        #Generate new prompt
-        if count == len(prompts)//2:
-            App.get_running_app().root.current = 'end'
+        if valid:        
+            #Generate new prompt
+            if idx == len(prompts)-1:
+                App.get_running_app().root.current = 'end'
             
-        new_prompt = prompts[count]
-        #Update Label
-        self.ids.prompt.text = new_prompt
+            #update label
+            idx +=1
+            new_prompt = prompts[idx][0]
+            new_label = "Good Job! Next Mission: "
+            self.ids.prompt.text = new_label+new_prompt
+        else:
+            self.ids.prompt.text = "Try again"
 
 class ThirdWindow(Screen):
     pass
@@ -69,7 +90,6 @@ class MyApp(App):
 if __name__ == "__main__":
     
     prompts = get_prompt()
-    print(prompts)
-    count = 0
+    idx = 0
     app = MyApp()
     app.run()
